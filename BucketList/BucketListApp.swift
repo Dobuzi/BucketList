08a138a15a054ee1da33e3258003ef38de87c68a
Dobuzi <kimjw7981@gmail.com>
2020-11-04 12:54:23 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by 김종원 on 2020/11/04.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
